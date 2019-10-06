95662c9bdd821118cbd0e2639a86f1621134e8f6
afrizalars <mohammad.afrizal@mail.ugm.ac.id>
2019-10-06 16:19:23 +0700

    final

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const { pool } = require('./config')
-// const authjwt = require('./middleware')
-// var jwt = require('jsonwebtoken');  
-// var bcrypt = require('bcryptjs');
+const authjwt = require('./middleware')
+var jwt = require('jsonwebtoken');  
+var bcrypt = require('bcryptjs');
 
-// //token auth
-// global.jwt = jwt
-// global.bcrypt = bcrypt
-// global.authjwt = authjwt
+//token auth
+global.jwt = jwt
+global.bcrypt = bcrypt
+global.authjwt = authjwt
 
 const app = express()
 
@@ -17,21 +17,19 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 
-// global.pool = pool
+global.pool = pool
 
-// const admin = require('./routes/admin')
-// const login = require('./routes/login')
+const admin = require('./routes/admin')
+const login = require('./routes/login')
 
-// app.get('/api/admin/getdriverlist',authjwt, admin.driverlist)
-// app.get('/api/admin/getwaitinglist_order',authjwt,admin.getwaitinglist_order)
-// app.get('/api/admin/getwaitinglist_order/gethistoryorder',authjwt,admin.gethistoryorder)
-// app.get('/api/admin/getwaitinglist_order/getlivetracking',authjwt,admin.getlivetracking)
-
-
-// app.post('/api/signup',login.signup)
-// app.post('/api/login',login.login)
+app.get('/api/admin/getdriverlist',authjwt, admin.driverlist)
+app.get('/api/admin/getwaitinglist_order',authjwt,admin.getwaitinglist_order)
+app.get('/api/admin/getwaitinglist_order/gethistoryorder',authjwt,admin.gethistoryorder)
+app.get('/api/admin/getwaitinglist_order/getlivetracking',authjwt,admin.getlivetracking)
 
 
+app.post('/api/signup',login.signup)
+app.post('/api/login',login.login)
 
 
 
@@ -40,32 +38,34 @@ app.use(cors())
 
 
 
-// const getBooks = (request, response) => {
-//   pool.query('SELECT * FROM books', (error, results) => {
-//     if (error) {
-//       throw error
-//     }
-//     response.status(200).json(results.rows)
-//   })
-// }
 
-// const addBook = (request, response) => {
-//   const { author, title } = request.body
 
-//   pool.query('INSERT INTO books (author, title) VALUES ($1, $2)', [author, title], error => {
-//     if (error) {
-//       throw error
-//     }
-//     response.status(201).json({ status: 'success', message: 'Book added.' })
-//   })
-// }
+const getBooks = (request, response) => {
+  pool.query('SELECT * FROM books', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
 
-// app
-//   .route('/books')
-//   // GET endpoint
-//   .get(getBooks)
-//   // POST endpoint
-//   .post(addBook)
+const addBook = (request, response) => {
+  const { author, title } = request.body
+
+  pool.query('INSERT INTO books (author, title) VALUES ($1, $2)', [author, title], error => {
+    if (error) {
+      throw error
+    }
+    response.status(201).json({ status: 'success', message: 'Book added.' })
+  })
+}
+
+app
+  .route('/books')
+  // GET endpoint
+  .get(getBooks)
+  // POST endpoint
+  .post(addBook)
 
 // Start server
 app.listen(process.env.PORT || 3002, () => {
