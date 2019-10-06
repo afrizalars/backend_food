f8c95f952842e258960676d99c14c46af82c17a5
afrizalars <mohammad.afrizal@mail.ugm.ac.id>
2019-10-06 16:10:11 +0700

    sasa

--- index.js
+++ index.js
@@ -40,32 +40,32 @@ app.post('/api/login',login.login)
 
 
 
-const getBooks = (request, response) => {
-  pool.query('SELECT * FROM books', (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-}
+// const getBooks = (request, response) => {
+//   pool.query('SELECT * FROM books', (error, results) => {
+//     if (error) {
+//       throw error
+//     }
+//     response.status(200).json(results.rows)
+//   })
+// }
 
-const addBook = (request, response) => {
-  const { author, title } = request.body
+// const addBook = (request, response) => {
+//   const { author, title } = request.body
 
-  pool.query('INSERT INTO books (author, title) VALUES ($1, $2)', [author, title], error => {
-    if (error) {
-      throw error
-    }
-    response.status(201).json({ status: 'success', message: 'Book added.' })
-  })
-}
+//   pool.query('INSERT INTO books (author, title) VALUES ($1, $2)', [author, title], error => {
+//     if (error) {
+//       throw error
+//     }
+//     response.status(201).json({ status: 'success', message: 'Book added.' })
+//   })
+// }
 
-app
-  .route('/books')
-  // GET endpoint
-  .get(getBooks)
-  // POST endpoint
-  .post(addBook)
+// app
+//   .route('/books')
+//   // GET endpoint
+//   .get(getBooks)
+//   // POST endpoint
+//   .post(addBook)
 
 // Start server
 app.listen(process.env.PORT || 3002, () => {
