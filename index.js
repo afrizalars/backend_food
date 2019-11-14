f2eb2bdaa85e2136acb203f914c8bc6134f6d12b
afrizalars <mohammad.afrizal@mail.ugm.ac.id>
2019-11-14 17:48:00 +0700

    ....

--- index.js
+++ index.js
@@ -19,76 +19,11 @@ app.use(cors())
 
 global.pool = pool
 
-const admin = require('./routes/admin')
-const login = require('./routes/login')
-const users = require('./routes/users')
 const features = require('./routes/features')
-
-//Admin
-app.get('/api/admin/insertDriver',authjwt,admin.insertDriver) //tested
-app.get('/api/admin/updateStatusDriver',authjwt,admin.updateStatusDriver) // tested
-app.get('/api/admin/getorder',authjwt,admin.getorder)
-app.post('/api/admin/approveOrder',authjwt,admin.approveOrder)
-
-app.get('/api/admin/cancelOrder', authjwt,admin.cancelOrder)
-
-
-app.get('/api/admin/getwaitinglist_order/getlivetracking',authjwt,admin.getlivetracking)
-
-//login signup
-app.post('/api/signup',login.signup) // tested
-app.post('/api/login',login.login) // tested
-app.get('/api/user_approval',authjwt,login.user_approval) // tested
-
-
-//features
-app.get('/api/all/getdriverlist',authjwt, features.driverlist)
-
-
-app.get('/api/all/getwaitinglist_order',authjwt,features.getwaitinglist_order)
-app.get('/api/all/gethistoryorder',authjwt,features.gethistoryorder)
-
-// //User
-app.post('/api/user/neworder',users.insertneworder)
-
 
 
 app.get('/get_foods_fact', features.getfoods_fact)
 app.get('/getfoods_fact_dev',features.getfoods_fact_dev)
-//"nodemon": "^1.19.3"
-
-
-
-
-
-
-
-// const getBooks = (request, response) => {
-//   pool.query('SELECT * FROM books', (error, results) => {
-//     if (error) {
-//       throw error
-//     }
-//     response.status(200).json(results.rows)
-//   })
-// }
-
-// const addBook = (request, response) => {
-//   const { author, title } = request.body
-
-//   pool.query('INSERT INTO books (author, title) VALUES ($1, $2)', [author, title], error => {
-//     if (error) {
-//       throw error
-//     }
-//     response.status(201).json({ status: 'success', message: 'Book added.' })
-//   })
-// }
-
-// app
-//   .route('/books')
-//   // GET endpoint
-//   .get(getBooks)
-//   // POST endpoint
-//   .post(addBook)
 
 // Start server
 app.listen(process.env.PORT || 5002, () => {
