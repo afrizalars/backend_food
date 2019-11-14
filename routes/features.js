d3c8aaac6ed1dcc0ea6524be47dc71cb6d9f8fa4
afrizalars <mohammad.afrizal@mail.ugm.ac.id>
2019-11-14 17:33:42 +0700

    ..

--- routes/features.js
+++ routes/features.js
@@ -1,7 +1,7 @@
 exports.getfoods_fact = function(req,res){
     if(req.method == "GET"){
 
-        var sql = "SELECT * FROM foods_fact order by index desc;"
+        var sql = "SELECT * FROM foods_fact_full order by index desc;"
         
         pool.query(sql , (error, results) => {
             if (error) {
